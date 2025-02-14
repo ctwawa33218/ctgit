26589acca5715728c9bff4f694f116648a58062c
ctyen <ctwawa33218@gmail.com>
2025-02-14 11:10:41 +0800

    Modify cherry3.

--- branch1.c
+++ branch1.c
@@ -3,7 +3,11 @@ int branch1()
 	printf("Modify\n");
 	printf("Modify\n");
 	printf("Add\n");
-
+	printf("Modify branch1\n");
+	printf("Modify branch1\n");
+	printf("Add cherry\n");	printf("Modify branch1\n");
+	printf("Modify branch1\n");
+	printf("Add cherry\n");
 
 	
 	printf("Modify branch1\n");
